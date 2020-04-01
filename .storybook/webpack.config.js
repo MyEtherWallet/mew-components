7d08be6cc017cbceaeb9489c05a6f79d7b713b08
Kosala Hemachandra <kvhnuke@aol.com>
2020-04-01 14:33:13 -0700

    remove clean

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,13 +2,8 @@ const path = require("path");
 
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
-  config.resolve = {
-    extensions: [".wasm", ".mjs", ".js", ".json", ".vue"],
-    alias: {
-      "@": path.resolve(__dirname, "../src"),
-      vue: 'vue/dist/vue.esm.js'
-    }
-  };
+  config.resolve.extensions.push(".vue");
+  config.resolve.alias["@"] = path.resolve(__dirname, "../src");
   // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
   // You can change the configuration based on that.
   // 'PRODUCTION' is used when building the static version of storybook.
