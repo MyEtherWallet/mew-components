e4a919ea471699ea0e363406c2ca954e4d4bf6b5
Jessica <jessicaspeng@gmail.com>
2020-05-05 14:31:47 -0700

    Remove links and update version

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  presets: ["@vue/app"]
+  presets: [
+    '@vue/app',
+    // {
+    //   /* Disabled due to clash between core-js versions in Storybook / Vue CLI */
+    //   useBuiltIns: false,
+    //   /* Disabled due to clash with Storybook MDX */
+    //   jsx: false
+    // }
+  ]
 };
