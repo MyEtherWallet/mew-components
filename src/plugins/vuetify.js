093661fa905904540842be68fa28d791b2e3091c
Jessica <jessicaspeng@gmail.com>
2020-05-05 13:09:05 -0700

    MEW component updates

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -16,18 +16,15 @@ import Vuetify, {
   VDialog,
   VDivider,
   VIcon,
-  VImg,
   VList,
   VListItem,
-  VListItemAvatar,
   VListItemContent,
   VListItemIcon,
   VListItemTitle,
   VNavigationDrawer,
   VRow,
   VSheet,
-  VBottomSheet,
-  VSparkline,
+  VBottomSheet
   VToolbar,
   VTextField,
   VLayout,
@@ -66,10 +63,8 @@ Vue.use(Vuetify, {
     VDialog,
     VDivider,
     VIcon,
-    VImg,
     VList,
     VListItem,
-    VListItemAvatar,
     VListItemContent,
     VListItemIcon,
     VListItemTitle,
@@ -77,7 +72,6 @@ Vue.use(Vuetify, {
     VRow,
     VSheet,
     VBottomSheet,
-    VSparkline,
     VToolbar,
     VTextField,
     VLayout,
@@ -139,51 +133,16 @@ export default new Vuetify({
         textPrimary: '#667f9b',
         white: '#ffffff',
         expandHeader: '#184f90',
-        primary: {
-          base: '#05c0a5',
-          lighten5: '#f5fdfb',
-          lighten4: '#f2fafa', 
-          lighten3: '#e4f1f1',
-          lighten2: '#dcfff9',
-          lighten1: '#baede6',
-          darken1: '#56c5b4',
-          darken2: '#00c0a5',
-          darken3: '#1eb19b',
-          darken4: '#0baa93',
-        },
-        secondary: {
-          hover: '#fff',
-          base: '#5a78f0',
-          lighten1: '#7895f2',
-          lighten2: '#7fdefb',
-          lighten3: '#d6dffa',
-          lighten4: '#f9fafe',
-          darken1: '#184f90'
-        },
-        basic: {
-          base: '#0b1a40',
-          lighten1: '#a9bcd2',
-          lighten3: '#f5f7f8',
-          lighten2: '#c5ccd3'
-        },
+        primary:'#05c0a5',
+        secondary:'#5a78f0',
+        basic:'#0b1a40',
         success: '#2cc479',
-        error: {
-          base: '#ff445b',
-          lighten1: '#ffd7dc',
-          lighten2: '#fff8f9'
-        },
+        error:'#ff445b',
         warning: {
           base: '#FFF2DC',
-          darken1: '#f5a623',
-          lighten2: '#ffa'
+          darken1: '#f5a623'
         },
-        disabled: {
-          base: '#cecece',
-          lighten5: 'fff',
-          lighten2: '#f2f4fa',
-          lighten1: '#e0e0e0',
-          darken1: '#999999'
-        }
+        disabled:'#cecece'
       },
       dark: {
         primaryHover: '#1eb19b',
@@ -215,58 +174,18 @@ export default new Vuetify({
         searchText: '#96a8b6',
         white: '#fff',
         expandHeader: '#184f90',
-        primary: {
-          base: '#05c0a5',
-          lighten5: '#f5fdfb',
-          lighten4: '#f2fafa', 
-          lighten3: '#e4f1f1',
-          lighten2: '#dcfff9',
-          lighten1: '#baede6',
-          darken1: '#56c5b4',
-          darken2: '#00c0a5',
-          darken3: '#1eb19b',
-          darken4: '#0baa93',
-        },
-        secondary: {
-          base: '#5c79f0',
-          lighten1: '#7895f2',
-          lighten2: '#7fdefb',
-          lighten3: '#d6dffa',
-          lighten4: '#f9fafe',
-          darken1: '#184f90'
-        },
-        basic: {
-          base: '#151A29',
-          lighten1: '#a9bcd2',
-          lighten2: '#f5f7f8',
-          lighten3: '#c5ccd3',
-          darken1: '#0d111e'
-        },
-        success: {
-          base: '#2cc479'
-        },
-        error: {
-          base: '#ff445b',
-          lighten1: '#ffd7dc',
-          lighten2: '#fff8f9'
-        },
+        titlePrimary: '#95aed8',
+        textPrimary: '#4e5a6e',
+        primary: '#05c0a5',
+        secondary: '#5c79f0',
+        basic:'#151A29',
+        success:'#2cc479',
+        error:'#ff445b',
         warning: {
           base: '#FFF2DC',
-          darken1: '#f5a623',
-          lighten2: '#ffa'
+          darken1: '#f5a623'
         },
-        yellow: {
-          base: '#ffb100'
-        },
-        disabled: {
-          base: '#1f2b42',
-          lighten5: 'fff',
-          lighten2: '#f2f4fa',
-          lighten1: '#e0e0e0',
-          darken1: '#999999'
-        },
-        titlePrimary: '#95aed8',
-        textPrimary: '#4e5a6e'
+        disabled: '#1f2b42'
       }
     }
   }
--- src/wrapper.js
+++ src/wrapper.js
@@ -1,5 +1,24 @@
 import button from './components/MewButton/MewButton.vue';
 import superButton from './components/MewSuperButton/MewSuperButton.vue';
+import addressSelect from './components/AddressSelect/AddressSelect.vue';
+import blockie from './components/Blockie/Blockie.vue';
+import infoCard from './components/InfoCard/InfoCard.vue';
+import infoTooltip from './components/InfoTooltip/InfoTooltip.vue';
+import mewCheckbox from './components/MewCheckbox/MewCheckbox.vue';
+import mewExpandPanel from './components/MewExpandPanel/MewExpandPanel.vue';
+import mewInput from './components/MewInput/MewInput.vue';
+import mewMenu from './components/MewMenu/MewMenu.vue';
+import mewModule from './components/MewModule/MewModule.vue';
+import mewOverlay from './components/MewOverlay/MewOverlay.vue';
+import mewPopup from './components/MewPopup/MewPopup.vue';
+import mewSelect from './components/MewSelect/MewSelect.vue';
+import mewStepper from './components/MewStepper/MewStepper.vue';
+import mewSwitch from './components/MewSwitch/MewSwitch.vue';
+import mewTabs from './components/MewTabs/MewTabs.vue';
+import toast from './components/Toast/Toast.vue';
+import txBadge from './components/TxBadge/TxBadge.vue';
+import txNotification from './components/TxNotification/TxNotification.vue';
+import warningSheet from './components/WarningSheet/WarningSheet.vue';
 
 // Declare install function executed by Vue.use()
 export function install(Vue) {
@@ -20,6 +39,7 @@ let GlobalVue = null;
 if (typeof window !== 'undefined') {
   GlobalVue = window.Vue;
 } else if (typeof global !== 'undefined') {
+  // eslint-disable-next-line no-undef
   GlobalVue = global.Vue;
 }
 if (GlobalVue) {
@@ -27,4 +47,23 @@ if (GlobalVue) {
 }
 
 export const MewButton = button,
-  MewSuperButton = superButton;
+  MewSuperButton = superButton,
+  Blockie = blockie,
+  InfoCard = infoCard,
+  InfoTooltip = infoTooltip,
+  MewCheckbox = mewCheckbox,
+  MewExpandPanel = mewExpandPanel,
+  MewInput = mewInput,
+  MewMenu = mewMenu,
+  MewModule = mewModule,
+  MewOverlay = mewOverlay,
+  MewPopup = mewPopup,
+  MewSelect = mewSelect,
+  MewStepper = mewStepper,
+  MewSwitch = mewSwitch,
+  MewTabs = mewTabs,
+  Toast = toast,
+  TxBadge = txBadge,
+  TxNotification = txNotification,
+  WarningSheet = warningSheet,
+  AddressSelect = addressSelect;
