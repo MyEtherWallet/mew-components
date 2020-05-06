4ace08aaa7c4024ed95f48694fbfb9c34370b767
Jessica <jessicaspeng@gmail.com>
2020-05-06 16:41:15 -0700

    Rebuilding dist folder

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,12 +1,12 @@
-import commonjs from "rollup-plugin-commonjs"; // Convert CommonJS modules to ES6
-import vue from "rollup-plugin-vue"; // Handle .vue SFC files
-import buble from "rollup-plugin-buble"; // Transpile/polyfill with reasonable browser support
+import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
+import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
 
 export default {
-  input: "src/wrapper.js", // Path relative to package.json
+  input: 'src/wrapper.js', // Path relative to package.json
   output: {
-    name: "MewButton",
-    exports: "named"
+    name: 'MewButton',
+    exports: 'named'
   },
   plugins: [
     commonjs(),
