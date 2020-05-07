2a9d6b429157137b18cc4a8283ae7ea46578f9a6
Jessica <jessicaspeng@gmail.com>
2020-05-07 12:30:51 -0700

    Fix rollup and update version

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify-es';
 import minimist from 'minimist';
+import resolve from '@rollup/plugin-node-resolve';
 
 // eslint-disable-next-line no-undef
 const argv = minimist(process.argv.slice(2));
@@ -12,7 +13,18 @@ const config = {
   input: 'src/wrapper.js', // Path relative to package.json
   output: {
     name: 'MewComponent',
-    exports: 'named'
+    exports: 'named',
+    globals: {
+      '@/components/Blockie/Blockie.vue': 'Blockie',
+      '@/components/Toast/Toast.vue': 'Toast',
+      '@/assets/images/icons/icon-copy-enable.png': 'IconCopy',
+      '@/assets/images/icons/icon-saved-enable.png': 'IconSave',
+      '@/helpers/blockies.js': 'Blockies',
+      '@/assets/images/icons/icon-edit.png': 'IconEdit',
+      '@/components/MewButton/MewButton.vue': 'MewButton',
+      '@/components/WarningSheet/WarningSheet.vue': 'WarningSheet',
+      '@/components/TxBadge/TxBadge.vue': 'TxBadge'
+    }
   },
   plugins: [
     replace({
@@ -26,8 +38,10 @@ const config = {
         isProduction: true
       }
     }),
-    buble()
-  ]
+    buble(),
+    resolve()
+  ],
+  external: ['@/components/TxBadge/TxBadge.vue', '@/components/MewButton/MewButton.vue', '@/components/WarningSheet/WarningSheet.vue', '@/components/Toast/Toast.vue', '@/components/Blockie/Blockie.vue', '@/assets/images/icons/icon-edit.png', '@/helpers/blockies.js', '@/assets/images/icons/icon-copy-enable.png', '@/assets/images/icons/icon-saved-enable.png']
 };
 
 // Only minify browser (iife) version
