e1bfa8206e07518d0f2c5fe41843db93fc5a1409
Jessica <jessicaspeng@gmail.com>
2020-05-07 10:46:28 -0700

    Update rollup config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,19 +1,38 @@
-import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
-import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
-import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
+import vue from 'rollup-plugin-vue';
+import buble from 'rollup-plugin-buble';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify-es';
+import minimist from 'minimist';
 
-export default {
+// eslint-disable-next-line no-undef
+const argv = minimist(process.argv.slice(2));
+
+const config = {
   input: 'src/wrapper.js', // Path relative to package.json
   output: {
-    name: 'MewButton',
+    name: 'MewComponent',
     exports: 'named'
   },
   plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     commonjs(),
     vue({
-      css: true, // Dynamically inject css as a <style> tag
-      compileTemplate: true // Explicitly convert template to render function
+      css: true,
+      compileTemplate: true,
+      template: {
+        isProduction: true
+      }
     }),
-    buble() // Transpile to ES5
+    buble()
   ]
 };
+
+// Only minify browser (iife) version
+if (argv.format === 'iife') {
+  config.plugins.push(uglify());
+}
+
+export default config;
