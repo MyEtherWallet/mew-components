d04041e8170aaed829f97284e4119a0fcdc2e8de
Jessica <jessicaspeng@gmail.com>
2020-05-29 12:01:13 -0700

    Testing

---
+++ src/helpers/copy.js
@@ -0,0 +1,11 @@
+function copyToClipboard(id) {
+  var copyText = document.getElementById(id);
+  var textArea = document.createElement('textarea');
+  textArea.value = copyText.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('Copy');
+  textArea.remove();
+}
+
+export default copyToClipboard;
--- src/wrapper.js
+++ src/wrapper.js
@@ -1,71 +1,78 @@
 import MewButton from './components/MewButton/MewButton.vue';
-import MewSuperButton from './components/MewSuperButton/MewSuperButton.vue';
-import AddressSelect from './components/AddressSelect/AddressSelect.vue';
-import Blockie from './components/Blockie/Blockie.vue';
-import InfoCard from './components/InfoCard/InfoCard.vue';
-import InfoTooltip from './components/InfoTooltip/InfoTooltip.vue';
-import MewCheckbox from './components/MewCheckbox/MewCheckbox.vue';
-import MewExpandPanel from './components/MewExpandPanel/MewExpandPanel.vue';
-import MewInput from './components/MewInput/MewInput.vue';
-import MewMenu from './components/MewMenu/MewMenu.vue';
-import MewModule from './components/MewModule/MewModule.vue';
-import MewOverlay from './components/MewOverlay/MewOverlay.vue';
-import MewPopup from './components/MewPopup/MewPopup.vue';
-import MewSelect from './components/MewSelect/MewSelect.vue';
-import MewStepper from './components/MewStepper/MewStepper.vue';
-import MewSwitch from './components/MewSwitch/MewSwitch.vue';
-import MewTabs from './components/MewTabs/MewTabs.vue';
-import Toast from './components/Toast/Toast.vue';
-import TxBadge from './components/TxBadge/TxBadge.vue';
-import TxNotification from './components/TxNotification/TxNotification.vue';
-import WarningSheet from './components/WarningSheet/WarningSheet.vue';
+// import MewSuperButton from './components/MewSuperButton/MewSuperButton.vue';
+// import AddressSelect from './components/AddressSelect/AddressSelect.vue';
+// import Blockie from './components/Blockie/Blockie.vue';
+// import InfoCard from './components/InfoCard/InfoCard.vue';
+// import InfoTooltip from './components/InfoTooltip/InfoTooltip.vue';
+// import MewCheckbox from './components/MewCheckbox/MewCheckbox.vue';
+// import MewExpandPanel from './components/MewExpandPanel/MewExpandPanel.vue';
+// import MewInput from './components/MewInput/MewInput.vue';
+// import MewMenu from './components/MewMenu/MewMenu.vue';
+// import MewModule from './components/MewModule/MewModule.vue';
+// import MewOverlay from './components/MewOverlay/MewOverlay.vue';
+// import MewPopup from './components/MewPopup/MewPopup.vue';
+// import MewSelect from './components/MewSelect/MewSelect.vue';
+// import MewStepper from './components/MewStepper/MewStepper.vue';
+// import MewSwitch from './components/MewSwitch/MewSwitch.vue';
+// import MewTabs from './components/MewTabs/MewTabs.vue';
+// import Toast from './components/Toast/Toast.vue';
+// import TxBadge from './components/TxBadge/TxBadge.vue';
+// import TxNotification from './components/TxNotification/TxNotification.vue';
+// import WarningSheet from './components/WarningSheet/WarningSheet.vue';
 
-const Components = {
-  MewButton,
-  MewSuperButton,
-  Blockie,
-  InfoCard,
-  InfoTooltip,
-  MewCheckbox,
-  MewExpandPanel,
-  MewInput,
-  MewMenu,
-  MewModule,
-  MewOverlay,
-  MewPopup,
-  MewSelect,
-  MewStepper,
-  MewSwitch,
-  MewTabs,
-  Toast,
-  TxNotification,
-  WarningSheet,
-  AddressSelect,
-  TxBadge
-}
+import Vue from 'vue';
+import wrap from '@vue/web-component-wrapper';
 
-// Declare install function executed by Vue.use()
-export function install() {
-  if (install.installed) return;
-  install.installed = true;
-}
+const CustomElement = wrap(Vue, MewButton);
 
-// Create module definition for Vue.use()
-const plugin = {
-  install
-};
+window.customElements.define('mew-button', CustomElement);
 
-// Auto-install when vue is found (eg. in browser via <script> tag)
-let GlobalVue = null;
+// const Components = {
+//   MewButton,
+//   MewSuperButton,
+//   Blockie,
+//   InfoCard,
+//   InfoTooltip,
+//   MewCheckbox,
+//   MewExpandPanel,
+//   MewInput,
+//   MewMenu,
+//   MewModule,
+//   MewOverlay,
+//   MewPopup,
+//   MewSelect,
+//   MewStepper,
+//   MewSwitch,
+//   MewTabs,
+//   Toast,
+//   TxNotification,
+//   WarningSheet,
+//   AddressSelect,
+//   TxBadge
+// }
 
-if (typeof window !== 'undefined') {
-  GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-  // eslint-disable-next-line no-undef
-  GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-  GlobalVue.use(plugin);
-}
+// // Declare install function executed by Vue.use()
+// export function install() {
+//   if (install.installed) return;
+//   install.installed = true;
+// }
 
-export default Components;
+// // Create module definition for Vue.use()
+// const plugin = {
+//   install
+// };
+
+// // Auto-install when vue is found (eg. in browser via <script> tag)
+// let GlobalVue = null;
+
+// if (typeof window !== 'undefined') {
+//   GlobalVue = window.Vue;
+// } else if (typeof global !== 'undefined') {
+//   // eslint-disable-next-line no-undef
+//   GlobalVue = global.Vue;
+// }
+// if (GlobalVue) {
+//   GlobalVue.use(plugin);
+// }
+
+// export default Components;
