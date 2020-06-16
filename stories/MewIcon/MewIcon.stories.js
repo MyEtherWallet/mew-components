dd33a8002f42bb818c82007209820c00b1dbcbd9
Jessica <jessicaspeng@gmail.com>
2020-06-15 17:32:04 -0700

    Update icon path

--- stories/MewIcon/MewIcon.stories.js
+++ stories/MewIcon/MewIcon.stories.js
@@ -21,8 +21,7 @@ export const MEWIcon = () => ({
   },
   data() {
     return {
-      footerIcons: ['btc', 'eth', 'facebook', 'github', 'instagram', 'linkedin', 'love', 'medium', 'reddit', 'twitter', 'youtube'], 
-      icons: ['aave', 'account', 'acct-value', 'advanced-tools', 'arrow', 'bcvault', 'bicycle', 'bitbox', 'buy-eth', 'calendar', 'car', 'chrome', 'clock', 'cool-wallet', 'copy', 'diploma', 'edit', 'ens-manager', 'eth-balance', 'finney', 'keepkey', 'keystore', 'ledger', 'lock', 'makerdao', 'message', 'mewtopia', 'mnemonic', 'nav-bar', 'network-nodes', 'new-wallet', 'night-mode', 'notification', 'paper-plane', 'private-key', 'quick-help', 'rocket', 'secalot', 'shield', 'swap-token', 'tags', 'team', 'thief', 'tokens-list', 'trezor', 'view-only', 'xwallet']
+      icons: ['aave', 'account', 'acctValue', 'advancedTools', 'arrow', 'bcvault', 'bicycle', 'bitbox', 'buyEth', 'calendar', 'car', 'chrome', 'clock', 'coolWallet', 'copy', 'diploma', 'edit', 'ensManager', 'ethBalance', 'finney', 'keepkey', 'keystore', 'ledger', 'lock', 'makerdao', 'message', 'mewtopia', 'mnemonic', 'navBar', 'networkNodes', 'newWallet', 'nightMode', 'notification', 'paperPlane', 'privateKey', 'quickHelp', 'rocket', 'secalot', 'shield', 'swapToken', 'tags', 'team', 'thief', 'tokensList', 'trezor', 'viewOnly', 'xwallet', 'btc', 'eth', 'facebook', 'github', 'instagram', 'linkedin', 'love', 'medium', 'reddit', 'twitter', 'youtube']
     }
   },
   watch: {
@@ -33,23 +32,12 @@ export const MEWIcon = () => ({
   template: `
     <div>
     <br />
-    <span class="mew-heading-1">SVG Icons</span>
+    <span class="mew-heading-1">Icons</span>
     <br />
     <v-row align="center"> 
       <v-col cols="2" v-for="(icon, idx) in icons" :key="icon + idx">
         <div class="d-flex justify-center flex-column">
           <mew-icon :icon-name="icon"/>
-          <span class="mew-caption">{{icon}}</span>
-        </div>
-      </v-col>
-    </v-row>
-    <br />
-    <span class="mew-heading-1">PNG Icons</span>
-    <br />
-    <v-row align="center"> 
-    <v-col cols="2" v-for="(icon, idx) in footerIcons" :key="icon + idx">
-        <div class="d-flex justify-center flex-column">
-          <mew-icon :img-height="30"  :is-png="true" :icon-name="icon"/>
           <span class="mew-caption">{{icon}}</span>
         </div>
       </v-col>
