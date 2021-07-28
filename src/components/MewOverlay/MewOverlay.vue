d8d06355aaf77d8bd9da845ff5a73a35fc34f886
Jessica <jessicaspeng@gmail.com>
2021-07-28 09:32:15 -0700

    overlay updates

--- src/components/MewOverlay/MewOverlay.vue
+++ src/components/MewOverlay/MewOverlay.vue
@@ -5,149 +5,152 @@
   =====================================================================================
   -->
   <v-bottom-sheet
-    :value="isOverlayOn" 
+    :value="isOverlayShown" 
     :fullscreen="true"
     persistent
   >
     <v-sheet 
-      height="100%" 
+      height="100%"
       color="overlayBg"
-      class="mew-overlay-container"
+      class="mew-overlay-container d-flex align-center flex-column "
     >
-      <v-container fluid>
-        <!--
+      <!--
   =====================================================================================
-    Mew Overlay action buttons and header title
-  =====================================================================================
-  -->
-        <v-row
-          class="icon-container"
-          align="center"
-          :justify="back ? 'space-between' : 'end'"
-        > 
-          <v-icon
-            v-if="back"
-            class="cursor-pointer d-flex align-center ma-5 pa-2"
-            @click="goBack"
-            size="16"
-            color="textBlack2"
-          >
-            mdi-arrow-left
-          </v-icon>
-          <v-icon
-            v-if="close"
-            class="cursor-pointer d-flex align-center ma-5 pa-2"
-            @click="closeOverlay"
-            size="16"
-            color="textBlack2"
-          >
-            mdi-close
-          </v-icon>
-        </v-row>
-        <v-row 
-          align="center"
-          class="pt-5 mb-3 mx-0"
-          justify="center"
-        >
-          <span
-            class="mew-subtitle titlePrimary--text text-center"
-          >{{ title }}</span>
-        </v-row>
-        <v-row 
-          v-if="description"
-          align="center"
-          class="mb-6 mx-0"
-          justify="center"
-        >
-          <span
-            class="mew-heading-3 titlePrimary--text font-weight-regular text-center"
-          >{{ description }}</span>
-        </v-row>
-        <!--
-  =====================================================================================
-    Mew Overlay Body, slot: mewOverlayBody (used to place custom ui in overlay body)
+    Mew Overlay action buttons (back and close)
   =====================================================================================
   -->
-        <div class="body-container d-flex flex-column align-center justify-center">
-          <slot name="mewOverlayBody" />
-          <mew-button
-            class="mt-4"
-            v-if="btnText"
-            color-theme="primary"
-            btn-type="background"
-            :title="btnText"
-          />
-          <mew-warning-sheet
-            v-if="warningTitle || warningDesc"
-            :title="warningTitle"
-            :description="warningDesc"
-          />
-        </div>
+      <v-row
+        class="icon-container full-width ma-0"
+        align="center"
+        :justify="back ? 'space-between' : 'end'"
+      > 
+        <v-icon
+          v-if="back"
+          class="cursor-pointer d-flex align-center ma-5 pa-2"
+          @click="back"
+          size="16"
+          color="textBlack2"
+        >
+          mdi-arrow-left
+        </v-icon>
+        <v-icon
+          v-if="close"
+          class="cursor-pointer d-flex align-center ma-5 pa-2"
+          @click="close"
+          size="16"
+          color="textBlack2"
+        >
+          mdi-close
+        </v-icon>
+      </v-row>
+      <v-container
+        ref="white-sheet-container"
+        class="pa-0 d-flex align-center justify-center flex-column"
+        fluid
+      >
+        <!--
+  =====================================================================================
+    White sheet (displays on the overlay - size is based on the contentSize prop)
+  =====================================================================================
+  -->
+        <v-row class="ma-0">
+          <v-sheet
+            ref="white-sheet-container"
+            :width="sheetWidth"
+            height="100%"
+            color="white"
+            class="white-sheet-container pa-8 mt-4"
+          >
+            <v-row 
+              align="center"
+              justify="center"
+              class="ma-0"
+            >
+              <!--
+    =====================================================================================
+    Title displayed on white sheet
+    =====================================================================================
+    -->
+              <span
+                class="mew-subtitle titlePrimary--text text-center"
+              >{{ title }}</span>
+              {{ hasScrollBar }}
+            </v-row>
+            <!--
+      =====================================================================================
+        Slot: used to place custom ui content
+      =====================================================================================
+      -->
+            <div class="d-flex flex-column align-center justify-center">
+              <slot />
+            </div>
+          </v-sheet>
+        </v-row>
+        <v-row class="ma-0 py-8 footer-text">
+          {{ footer.text }} 
+          <a
+            v-if="footer && footer.linkTitle && footer.link"
+            rel="noopener noreferrer"
+            class="cursor-pointer font-weight-medium ml-1"
+            :href="footer.link"
+            target="_blank"
+          >
+            {{ footer.linkTitle }}
+          </a>
+        </v-row>
       </v-container>
     </v-sheet>
   </v-bottom-sheet>
 </template>
 
 <script>
-import MewButton from '@/components/MewButton/MewButton.vue';
-import MewWarningSheet from '@/components/MewWarningSheet/MewWarningSheet.vue';
+const sizes = {
+  mobile: 'mobile',
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+  xlarge: 'xlarge'
+}
 
 export default {
   name: 'MewOverlay',
   data() {
     return {
-      isOverlayOn: false
+      isOverlayShown: false
     }
-  },
-  components: {
-    MewButton,
-    MewWarningSheet
   },
   props: {
     /**
-     * Shows the overlay.
+     * Displays on the outside bottom of the white sheet.
+     * takes an object, i.e {text: 'Need help?', linkTitle: 'Contact support',
+     * link: 'mailto:support@myetherwallet.com'}
+     */
+    footer: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    /**
+     * Opens the overlay from 
+     * the bottom of the screen.
      */
     showOverlay: {
       type: Boolean,
       default: false
     },
     /**
-     * Title of overlay.
+     * Overlay title.
+     * Displays on the white sheet.
      */
     title: {
       type: String,
       default: ''
     },
     /**
-     * Description of overlay.
-     */
-    description: {
-      type: String,
-      default: ''
-    },
-    /**
-     * Displays and shows the button text.
-     */
-    btnText: {
-      type: String,
-      default: ''
-    },
-    /**
-     * Displays and shows the warning title.
-     */
-    warningTitle: {
-      type: String,
-      default: ''
-    },
-    /**
-     * Displays and shows the warning description.
-     */
-    warningDesc: {
-      type: String,
-      default: ''
-    },
-    /**
-     * Close button.
+     * Function that gets triggered 
+     * by close icon on the right.
+     *
      */
     close: {
       type: Function,
@@ -156,31 +159,59 @@ export default {
       }
     },
     /**
-     * Back button.
+     * Function that gets triggered
+     * by left arrow icon on the left.
      */
     back: {
       type: Function,
       default: () => {
         return {};
       }
+    },
+    /**
+     * Applies the size of the white sheet on the overlay.
+     * options: 'mobile' - < 344, 'small' - 384px, 
+     * 'medium' - 504px, 'large' - 624px, 'xlarge' - 744px.
+     */
+    contentSize: {
+      type: String,
+      default: 'small'
+    },
+  },
+  computed: {
+    sheetWidth() {
+      if (this.contentSize) {
+        switch(this.contentSize.toLowerCase()) {
+          case sizes.mobile || this.$vuetify.breakpoint.xs:
+            return '100%';
+          case sizes.small: 
+            return '384px';
+          case sizes.medium: 
+            return '504px';
+          case sizes.large: 
+            return '624px';
+          case sizes.xlarge:
+            return '744px';
+          default: 
+            return '384px';
+        }
+      }
+      return '384px';
+    },
+    hasScrollBar() {
+
+      return '';
     }
   },
   watch: {
     showOverlay(newVal) {
-      this.isOverlayOn = newVal;
+          console.error('adsfadsf', this.$refs)
+
+      this.isOverlayShown = newVal;
     }
   },
   mounted() {
-    this.isOverlayOn = this.showOverlay;
-  },
-  methods: {
-    closeOverlay() {
-      this.$emit('closeOverlay')
-      this.close();
-    },
-    goBack() {
-      this.back();
-    }
+    this.isOverlayShown = this.showOverlay;
   }
 }
 </script>
@@ -188,11 +219,20 @@ export default {
 <style lang="scss" scoped>
 .mew-overlay-container {
   overflow: auto;
+  .white-sheet-container {
+    border-radius: 12px;
+    box-shadow: 0px 12px 17px rgba(21, 29, 63, 0.0332441);
+  }
   .icon-container {
+    position: absolute;
+    top: 0;
     .v-icon:hover {
     background-color: rgba(95, 99, 104, 0.06);
     border-radius: 50%;
     }
+  }
+  .footer-text {
+    color: rgba(11, 40, 64, 0.72);
   }
 }
 </style>
--- stories/MewOverlay/MewOverlay.stories.js
+++ stories/MewOverlay/MewOverlay.stories.js
@@ -1,7 +1,9 @@
 import {
   withKnobs,
   boolean,
-  text
+  text,
+  optionsKnob,
+  object
 } from '@storybook/addon-knobs';
 import MewOverlay from '@/components/MewOverlay/MewOverlay.vue';
 
@@ -12,6 +14,18 @@ export default {
   },
   decorators: [withKnobs]
 };
+
+const options = {
+  display: 'inline-radio'
+};
+
+const sizes = {
+  mobile: 'mobile',
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+  xlarge: 'xlarge'
+}
 
 export const MEWOverlay = () => ({
   data() {
@@ -30,23 +44,17 @@ export const MEWOverlay = () => ({
     title: {
       default: text('title', 'Title')
     },
-    btnText: {
-      default: text('btn-text', '')
+    contentSize: {
+      default: optionsKnob(
+        'content-size',
+        sizes,
+        '',
+        options
+      )
     },
-    warningTitle: {
-      default: text('warning-title', '')
-    },
-    warningDesc: {
-      default: text('warning-desc', '')
-    },
-    rightBtnText: {
-      default: text('right-btn-text', 'Cancel')
-    },
-    leftBtnText: {
-      default: text('left-btn-text', 'Back')
-    },
-    description: {
-      default: text('description', '')
+    footer: {
+      default: object('footer-text', {text: 'Need help?', linkTitle: 'Contact support',
+      link: 'mailto:support@myetherwallet.com'})
     }
   },
   watch: {
@@ -73,18 +81,18 @@ export const MEWOverlay = () => ({
     <mew-overlay
       :back="back"
       :close="close"
-      :show-overlay="showsOverlay"
+      :footer="footer"
+      :content-size="contentSize"
+      :show-overlay="showOverlay"
       :title="title"
-      :btn-text="btnText"
-      :warning-title="warningTitle"
-      :warning-desc="warningDesc"
-      :right-btn-text="rightBtnText"
-      :left-btn-text="leftBtnText"
-      :description="description"
     >
-      <template v-slot:mewOverlayBody>
-        <span>MEW overlay body</span>
-      </template>
+      <span>MEW overlay body</span>
+      <div>
+      Where does it come from?
+Contrary to popular belief,Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dumm  Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dumm
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+      </div>
     </mew-overlay>
   </div>`
 });
