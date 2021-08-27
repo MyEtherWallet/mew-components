379a53933cbc97755e9a8e15eda79f4b955a11d3
Jessica <jessicaspeng@gmail.com>
2021-08-26 17:01:11 -0700

    mew dropdown

---
+++ stories/MewDropdown/MewDropdown.stories.js
@@ -0,0 +1,54 @@
+import {
+  withKnobs,
+  boolean,
+  text
+} from '@storybook/addon-knobs';
+import MewDropdown from '@/components/MewDropdown/MewDropdown.vue';
+
+export default {
+  title: 'MewDropdown',
+  parameters: {
+    component: MewDropdown
+  },
+  decorators: [withKnobs]
+};
+
+
+export const MEWDropdown = () => ({
+  components: { MewDropdown },
+  props: {
+    enableDarkMode: {
+      default: boolean('dark mode ?', false)
+    },
+    popupTitle: {
+      default: text('popupTitle', 'Select a derivation path')
+    },
+    isOpened: {
+      default: boolean('isOpened', false)
+    }
+  },
+  watch: {
+    enableDarkMode(newVal) {
+      this.$vuetify.theme.dark = newVal === true ? true : false;
+    }
+  },
+  template: `
+    <div>
+    <br />
+    <mew-dropdown
+      :is-opened="isOpened"
+      :popup-title="popupTitle"
+    >
+      <template #activatorBtnText>
+        <span class="titlePrimary--text mew-body capitalize">
+          Ethereum 
+          
+          </span>
+        <span class="textSecondary--text mew-body capitalize">M/44’/60’/0’</span>
+      </template>
+      <template #cardContent>
+        <div>Card Content Slot</div>
+      </template>
+    </mew-dropdown>
+  </div>`
+});
