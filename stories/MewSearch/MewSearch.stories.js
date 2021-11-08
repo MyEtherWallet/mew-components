517cf2e8eb69b1572e7f66b1dad1b1d601a63a39
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-11-08 15:48:19 -0800

    chore: add type to story

--- stories/MewSearch/MewSearch.stories.js
+++ stories/MewSearch/MewSearch.stories.js
@@ -1,64 +1,90 @@
-import { withKnobs, object, text, boolean } from '@storybook/addon-knobs';
-import MewSearch from '@/components/MewSearch/MewSearch.vue';
+import {
+  withKnobs,
+  object,
+  text,
+  boolean,
+  optionsKnob,
+} from "@storybook/addon-knobs";
+import MewSearch from "@/components/MewSearch/MewSearch.vue";
 
 export default {
-  title: 'MewSearch',
+  title: "MewSearch",
   parameters: {
-    component: MewSearch
+    component: MewSearch,
   },
-  decorators: [withKnobs]
+  decorators: [withKnobs],
 };
 
 const menuSelect = {
-  label: 'Extensions',
-  items: [ { name: '.crypto', value: '.crypto' }]
-}
+  label: "Extensions",
+  items: [{ name: ".crypto", value: ".crypto" }],
+};
+
+const textInputTypes = {
+  tel: "tel",
+  text: "text",
+  time: "time",
+  url: "url",
+  week: "week",
+  month: "month",
+  number: "number",
+  password: "password",
+  datetime: "datetime-local",
+  email: "email",
+};
+
+const optionsObj = {
+  display: "inline-radio",
+};
 
 export const MEWSearch = () => ({
   components: { MewSearch },
   props: {
     disabled: {
-      default: boolean('disabled', false)
+      default: boolean("disabled", false),
     },
     placeholder: {
-      default: text('placeholder', 'placeholder')
+      default: text("placeholder", "placeholder"),
     },
     value: {
-      default: text('value', '')
+      default: text("value", ""),
     },
     isSearchBlock: {
-      default: boolean('is-search-block', true)
+      default: boolean("is-search-block", true),
     },
     menuSelect: {
-      default: object('menu-select', menuSelect)
+      default: object("menu-select", menuSelect),
     },
     isCompact: {
-      default: boolean('is-compact', false)
+      default: boolean("is-compact", false),
     },
     isFilled: {
-      default: boolean('is-filled', true)
+      default: boolean("is-filled", true),
     },
     enableDarkMode: {
-      default: boolean('dark mode ?', false)
+      default: boolean("dark mode ?", false),
     },
     errorMessages: {
-      default: text('error-messages', '')
-    }
+      default: text("error-messages", ""),
+    },
+    type: {
+      default: optionsKnob("type", textInputTypes, "text", optionsObj),
+    },
   },
   watch: {
     enableDarkMode(newVal) {
       this.$vuetify.theme.dark = newVal === true ? true : false;
-    }
+    },
   },
   methods: {
     onSearch() {
-      console.log('search button clicked')
-    }
+      console.log("search button clicked");
+    },
   },
   template: `
     <div>
     <br />
-    <mew-search :on-search="onSearch" :is-filled="isFilled" :error-messages="errorMessages" :is-compact="isCompact" :menu-select="menuSelect" :is-search-block="isSearchBlock" :disabled="disabled" :placeholder="placeholder" :value="value"
+    <mew-search :on-search="onSearch" :is-filled="isFilled" :error-messages="errorMessages" :is-compact="isCompact" :menu-select="menuSelect" :is-search-block="isSearchBlock" :disabled="disabled" :placeholder="placeholder" :value="value" :type="type"
     />
-  </div>`
+  </div>`,
 });
