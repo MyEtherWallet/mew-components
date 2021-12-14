36d39431152639eb90293e0b960ac9f64d159995
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-12-14 15:01:31 -0800

    chore: run lint

--- src/components/MewSearch/MewSearch.vue
+++ src/components/MewSearch/MewSearch.vue
@@ -68,8 +68,15 @@
           mdi-magnify
         </v-icon>
       </v-btn>
-      <v-divider v-if="isSearchBlock && canSearchDate" vertical light />
-      <div class="pl-3 pr-4" v-if="isSearchBlock && canSearchDate">
+      <v-divider
+        v-if="isSearchBlock && canSearchDate"
+        vertical
+        light
+      />
+      <div
+        class="pl-3 pr-4"
+        v-if="isSearchBlock && canSearchDate"
+      >
         <mew-icon-button
           btn-style="transparent"
           mdi-icon-size="medium"
@@ -90,9 +97,9 @@
 </template>
 
 <script>
-import MewIconButton from "@/components/MewIconButton/MewIconButton.vue";
+import MewIconButton from '@/components/MewIconButton/MewIconButton.vue';
 export default {
-  name: "MewSearch",
+  name: 'MewSearch',
   props: {
     /**
      * Click to search method for search button
@@ -122,14 +129,14 @@ export default {
      */
     placeholder: {
       type: String,
-      default: "",
+      default: '',
     },
     /**
      * The input value.
      */
     value: {
       type: String,
-      default: "",
+      default: '',
     },
     /**
      * Displays an outline around input
@@ -177,7 +184,7 @@ export default {
      */
     errorMessages: {
       type: [String, Array],
-      default: "",
+      default: '',
     },
     /**
      * Input types
@@ -185,7 +192,7 @@ export default {
      */
     type: {
       type: String,
-      default: "search",
+      default: 'search',
     },
   },
   components: {
@@ -193,7 +200,7 @@ export default {
   },
   data() {
     return {
-      inputValue: "",
+      inputValue: '',
       menuSelectModel: {},
     };
   },
@@ -203,26 +210,26 @@ export default {
      */
     searchHeight() {
       if (this.isCompact && !this.isSearchBlock) {
-        return "36px";
+        return '36px';
       }
       if (this.isCompact && this.isSearchBlock) {
-        return "46px";
+        return '46px';
       }
-      return "62px";
+      return '62px';
     },
     /**
      * @returns classes for mew search - needed for styling
      */
     mewSearchClasses() {
-      const classes = ["mew-search"];
+      const classes = ['mew-search'];
       this.isSearchBlock
-        ? classes.push("search-block")
-        : classes.push("search-standard");
+        ? classes.push('search-block')
+        : classes.push('search-standard');
       if (!this.isCompact) {
-        classes.push("search-large");
+        classes.push('search-large');
       }
       if (this.isFilled) {
-        classes.push("search-filled");
+        classes.push('search-filled');
       }
       return classes;
     },
@@ -233,7 +240,7 @@ export default {
      */
     inputValue(newVal, oldVal) {
       if (newVal !== oldVal) {
-        this.$emit("input", newVal);
+        this.$emit('input', newVal);
       }
     },
     /**
@@ -257,7 +264,7 @@ export default {
      * will @emit menu-select with value to parent container
      */
     onMenuSelect() {
-      this.$emit("menu-select", this.menuSelectModel);
+      this.$emit('menu-select', this.menuSelectModel);
     },
   },
 };
