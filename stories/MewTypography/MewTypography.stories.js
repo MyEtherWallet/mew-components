60f8f03d10435d0bc257c52a19a6c889ceb25ec9
Jessica Peng <jpeng@ylopo.com>
2022-01-19 20:15:18 -0800

    remove line height text

--- stories/MewTypography/MewTypography.stories.js
+++ stories/MewTypography/MewTypography.stories.js
@@ -101,31 +101,31 @@ export const All = () => ({
     <div :style="mewBody, { fontWeight: 700 }">.font-weight-bold</div>
     <div :style="markupStyle">font-weight: 700</div>
     <div :style="mewTitle">.mew-title</div>
-    <div :style="markupStyle">font-size: 3.571rem (50px); line-height: 3.714rem; font-weight: 700</div>
+    <div :style="markupStyle">font-size: 3.571rem (50px); font-weight: 700</div>
     <br />
     <div :style="mewSubtitle">.mew-subtitle</div>
-    <div :style="markupStyle">font-size: 2.143rem (30px); font-weight: 700; line-height: 2.857rem</div>
+    <div :style="markupStyle">font-size: 2.143rem (30px); font-weight: 700;</div>
     <br />
     <div :style="mewHeading1">.mew-heading-1</div>
-    <div :style="markupStyle">font-size: 1.714rem (24px), font-weight: 700, line-height: 2.286rem</div>
+    <div :style="markupStyle">font-size: 1.714rem (24px); font-weight: 700</div>
     <br />
     <div :style="mewHeading2">.mew-heading-2</div>
-    <div :style="markupStyle">font-size: 1.429rem (20px), line-height: 2rem, font-weight: 700</div>
+    <div :style="markupStyle">font-size: 1.429rem (20px); font-weight: 700</div>
     <br />
     <div :style="mewHeading3">.mew-heading-3</div>
-    <div :style="markupStyle">font-size: 1.143rem (16px), font-weight: 700, line-height: 1.714rem</div>
+    <div :style="markupStyle">font-size: 1.143rem (16px); font-weight: 700</div>
     <br />
     <div :style="mewHeading4">.mew-heading-4</div>
-    <div :style="markupStyle">font-size: 1.143rem (16px); font-weight: 400; line-height: 1.714rem </div>
+    <div :style="markupStyle">font-size: 1.143rem (16px); font-weight: 400 </div>
     <br />
     <div :style="mewBody">.mew-body</div>
-    <div :style="markupStyle">font-size: 1rem (14px), font-weight: 400, line-height: 1.429rem</div>
+    <div :style="markupStyle">font-size: 1rem (14px); font-weight: 400</div>
     <br />
     <div :style="mewCaption">.mew-caption</div>
-    <div :style="markupStyle">font size: 0.8571rem (12px), line-height: 1.429rem, font-weight: 700, text-transform: uppercase</div>
+    <div :style="markupStyle">font size: 0.8571rem (12px); font-weight: 700; text-transform: uppercase</div>
     <br />
     <div :style="mewLabel">.mew-label</div>
-    <div :style="markupStyle">font-size: 0.8571rem (12px); font-weight: 400; line-height: 1.143rem</div>
+    <div :style="markupStyle">font-size: 0.8571rem (12px); font-weight: 400</div>
     <br />
     <div :style="capitalize">.capitalize</div>
     <div :style="markupStyle">text-transform: capitalize</div>
