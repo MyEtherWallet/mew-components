6599f10af4ce4901f8297d91c7c56f6367b03440
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-06-02 12:06:22 -0700

    chore: fix build

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,11 @@
 module.exports = {
-  // assetsDir: '/mew-components/',
-  // publicPath: './', // Base directory for dev
-  // css: {
-  //   sourceMap: true,
-  //   loaderOptions: {
-  //     sass: {
-  //       data: '@import "@/assets/styles/global.scss";'
-  //     }
-  //   }
-  // },
-  // chainWebpack: config => {
-  //   config.module
-  //     .rule('fonts')
-  //     .test(/\.(ttf|otf|eot|woff|woff2)$/)
-  //     .use('file-loader')
-  //       .loader('file-loader')
-  //       .tap(options => {
-  //         options = {
-  //           // limit: 10000,
-  //           name: '/assets/fonts/[name].[ext]',
-  //         }
-  //         return options
-  //       })
-  //       .end()
-  // },
   pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: []
-    }
-  }
-}
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [],
+    },
+  },
+  buble: {
+    objectAssign: "Object.assign",
+  },
+};
