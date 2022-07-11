b2970653b5d157eda4d8d0ca9277909c1875e093
David Hong <hsurf22@gmail.com>
2022-07-10 20:38:09 -0700

    devop: add cloear method

--- src/components/MewAddressSelect/MewAddressSelect.vue
+++ src/components/MewAddressSelect/MewAddressSelect.vue
@@ -274,6 +274,12 @@ export default {
   },
   methods: {
     /**
+     * Clears the v-model value.
+     */
+    clear() {
+      this.addressValue = '';
+    },
+    /**
      * Emits 'saveAddress' when triggered by save address button.
      */
     saveAddress() {
