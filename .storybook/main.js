e64180369389a3208ad1f29ef561eba51a2c2433
andrewdahn <andrewahnjr@gmail.com>
2022-10-11 15:53:53 -0700

    devop: update main.js for vue3 and address glob warning

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.(js|mdx)'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-backgrounds/register', '@storybook/addon-docs', '@storybook/addon-knobs/register', '@storybook/addon-viewport/register', '@storybook/addon-links/register'],
+  stories: ['../stories/**/*.stories.@(js|mdx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-docs',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-links/register',
+  ],
 };
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
-import Vue from 'vue';
+import { createApp } from 'vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h()
-}).$mount('#app');
+const app = createApp({
+  render: (h) => h(),
+});
+
+app.use(router);
+app.use(store);
+app.use(vuetify);
+app.mount('#app');
