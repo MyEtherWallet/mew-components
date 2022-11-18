5b7cf53d65f468c653940fc6f57c04f43fa0ee6a
David Hong <davidh@myetherwallet.com>
2022-11-17 16:11:46 -0800

    fix: mew-overlay double scrollbar bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,54 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true,
-        'node': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:vue/essential',
-        'plugin:vue/strongly-recommended',
-        'plugin:storybook/recommended'
+  globals: {
+    VERSION: 'readonly',
+    ROUTER_MODE: 'readonly',
+    NODE_ENV: 'readonly'
+  },
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/prettier',
+    'eslint:recommended',
+    'plugin:security/recommended'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-else-return': ['error', { allowElseIf: true }],
+    'arrow-parens': 'off',
+    'generator-star-spacing': 'off',
+    semi: 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'security/detect-new-buffer': 'off',
+    'security/detect-object-injection': 'off',
+    'require-atomic-updates': 'off',
+    'no-prototype-builtins': 'off',
+    'no-irregular-whitespace': [
+      'error',
+      {
+        skipComments: true,
+        skipTemplates: true,
+        skipStrings: true,
+        skipRegExps: true
+      }
     ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'vue'
-    ],
-    'rules': {
-        'quotes': ['error', 'single']
-    }
+    'vue/custom-event-name-casing': 'off',
+    'vue/no-unused-properties': [
+      'error',
+      {
+        groups: ['props', 'data', 'computed', 'methods', 'setup'],
+        deepData: false,
+        ignorePublicMembers: false
+      }
+    ]
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  plugins: ['security']
 };
