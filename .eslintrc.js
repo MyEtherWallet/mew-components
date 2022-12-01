e4664d3da6d7e56eaf51e1bd6b995762e65d5c6e
David Hong <davidh@myetherwallet.com>
2022-11-30 22:28:46 -0800

    devop: rever eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true
-  },
-  'extends': [
-    'eslint:recommended',
-    'plugin:vue/essential',
-    'plugin:vue/strongly-recommended',
-    'plugin:storybook/recommended'
-  ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
-  },
-  'parserOptions': {
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
-  },
-  'plugins': [
-    'vue'
-  ],
-  'rules': {
-    'quotes': ['error', 'single']
-  }
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:vue/essential',
+        'plugin:vue/strongly-recommended',
+        'plugin:storybook/recommended'
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'vue'
+    ],
+    'rules': {
+        'quotes': ['error', 'single']
+    }
 };
